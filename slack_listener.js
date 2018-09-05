1912cd4202e644710453b1a57024342ff4153ac2
root <root@LAPTOP-LQRBCNTA.localdomain>
2018-09-05 11:27:47 +0530

    Added slack_listener.js flintbit

---
+++ slack_listener.js
@@ -0,0 +1,158 @@
+/**
+** Creation Date: 4th September 2018
+** Summary: This is Slack-Flint Bot, Message Listener flintbit.
+** Description: This flintbit is developed to listen to slack messages/responses containing Flint triggers-words.
+**/
+
+log.trace("Started execution of 'example:slack_listener.js' flintbit.")
+try {
+    key_values = []
+    // parse URL encoded data to json
+    message = input.get('body') // Flint body field to get slack data
+    message_body = message.body.replace(/[\"]/g, '').split('&')
+
+    for (index in message_body) {
+        key_values.push(message_body[index].split('='))
+    }
+
+    // Getting slack request variables and putting them in JS variables
+    token = key_values[0][1]
+    team_id = key_values[1][1]
+    team_domain = key_values[2][1]
+    service_id = key_values[3][1]
+    channel_id = key_values[4][1]
+    channel_name = key_values[5][1]
+    user_id = key_values[7][1]
+    user_name = key_values[8][1]
+    text = key_values[9][1]
+    trigger_word = key_values[10][1]
+
+    // Command
+    command = key_values[9][1].replace(/[+]/g, ' ').split(' ')
+
+    // Getting VM details from command
+    provider = command[0]
+    image_type = command[1]
+    instance_type = command[2]
+    region = command[3]
+    availability_zone = command[4]
+
+    slack_chat = 'slack'
+    id = token
+
+    // Inputs: Service config->slack_listener.js->add_message.js
+    
+    url = input.get('url')  // #demo on Slack url
+    http_connector_name = input.get('http_connector_name')
+    method = input.get('method')
+
+    // // Getting values from slack data
+    // body = message_body['text'][0].delete '"'
+    // command = message_body['command'][0]
+    // channel_name = message_body['channel_name'][0]
+    // user_name = message_body['user_name'][0]
+    // id = message_body['"token'][0]
+    // message_data = body.split(' ')
+
+    // Initial notification to slack from FlintBot
+    //http_body = '{"channel": "#' + channel_name + '", "username": "FlintBot", "text": "Hello ' + user_name + ', I got your request and started processing it"}'
+
+    acknowledgement_body = '{"channel": "#' + channel_name + '", "text": "Hello ' + user_name + ',. I got your request and started processing it"}'
+
+    call.bit('hipchat-terraform:operation:add_message.js')
+        .set('body', acknowledgement_body)
+        .set('chat_tool', slack_chat)
+        .set('url', url)
+        .set('method', method)
+        .set('http_connector_name', http_connector_name)
+        .sync()
+
+    // if message_data.length >= 2
+    //     provider = message_data[0]
+    // image_type = message_data[1]
+    // instance_type = message_data[2]
+    // region = message_data[3]
+    // availability_zone = message_data[4]
+
+    if (command != null || command != '' && provider == "aws") {
+        switch (command) {
+            case 'newvm':
+                log.trace('Calling Flintbit to perform newvm Operation')
+                call.bit('hipchat-terraform:operation:newvm.rb')
+                    .set('id', id)
+                    .set('image_type', image_type)
+                    .set('channel_name', channel_name)
+                    .set('provider', provider)
+                    .set('instance_type', instance_type)
+                    .set('channel_name', channel_name)
+                    .set('availability_zone', availability_zone)
+                    .set('chat_tool', slack_chat)
+                    .set('region', region)
+                    .set('user_name', user_name)
+                    .sync()
+                    break;
+
+            case 'startvm':
+                log.trace('Calling Flintbit to perform startawsvm Operation')
+                call.bit('hipchat-terraform:operation:startawsvm.rb')
+                    .set('id', id)
+                    .set('instance_id', image_type)
+                    .set('provider', provider)
+                    .set('chat_tool', slack_chat)
+                    .set('region', instance_type)
+                    .set('user_name', user_name)
+                    .sync()
+                    break;
+
+            case 'stopvm':
+                log.trace('Calling Flintbit to perform stopawsvm Operation')
+                call.bit('hipchat-terraform:operation:stopawsvm.rb')
+                    .set('id', id)
+                    .set('instance_id', image_type)
+                    .set('provider', provider)
+                    .set('chat_tool', slack_chat)
+                    .set('region', instance_type)
+                    .set('user_name', user_name)
+                    .sync()
+                    break;
+
+            case 'destroyvm':
+                call.bit('hipchat-terraform:operation:destroyvm.rb')
+                    .set('id', id)
+                    .set('region', instance_type)
+                    .set('provider', provider)
+                    .set('operation', command)
+                    .set('channel_name', channel_name)
+                    .set('instance_id', image_type)
+                    .set('chat_tool', slack_chat)
+                    .set('user_name', user_name)
+                    .sync()
+                    break;
+        }
+    }
+    else {
+        if(provider != "aws"){
+            slack_reply_message = 'Hello '+user_name+', please set a valid provider(aws).'
+        }
+        else if(command.length < 5){
+            slack_reply_message = 'Hello ' + user_name + ', The command has missing parameters.\n <b>Syntax:</b> \n<provider> <image-type> <instance-type> <region> <availability-zone>'
+        }
+    }
+} catch (error) {
+    log.error(error.message)
+    output.set('exit-code', 1).set('message', error.message)
+
+    slack_reply_message = 'Hello ' + user_name + '. Something went wrong, please try again ' + error.message.toString()
+    // body = '{"channel": "#' + channel_name + '", "username": "FlintBot", "text": "' + slack_reply_message + '"}'
+    body = '{"channel": "#' + channel_name + '", "text": "' + slack_reply_message + '"}'
+    
+    call.bit('hipchat-terraform:operation:add_message.js')
+        .set('body', body)
+        .set('chat_tool', slack_chat)
+        .set('url', url)
+        .set('method', method)
+        .set('http_connector_name', http_connector_name)
+        .sync()
+}
+
+log.trace("Finished execution of 'example:slack_listener.js' flintbit.")
