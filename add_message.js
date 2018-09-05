c8fb9b116298f7ff84979ca257704434d45bb509
root <root@LAPTOP-LQRBCNTA.localdomain>
2018-09-05 12:44:54 +0530

    Updating add_message.js flintbit

--- add_message.js
+++ add_message.js
@@ -13,18 +13,18 @@ try {
     // Getting http connector inputs from slack_listener
     http_connector_name = input.get('http_connector_name')
     method = input.get('method')
-    //headers = config.global('slack-terraform.terraform.http_connector.header')
+    headers = config.global('slack-terraform.terraform.http_connector.header')
     url = input.get('url')
 
-    log.info("Flintbit Input Parameters: \nConnector Name:" + http_connector_name + "\n Method:" + method + "\nBody:" + body )
-    //+ "\nHeaders: " + headers
+    log.info("Flintbit Input Parameters: \nConnector Name:" + http_connector_name + "\n Method:" + method + "\nBody:" + body+ "\nHeaders: " + headers )
+    
 
     log.info('Calling HTTP connector to notify status')
     connector_response = call.connector(http_connector_name)
         .set('method', method)
         .set('url', url)                        // url(slack channel #demo)::service config->slack_listener.js->add_message.js
         .set('body', body)
-        //.set('headers', headers)
+        .set('headers', headers)
         .sync()
 
     response_exitcode = connector_response.exitcode
